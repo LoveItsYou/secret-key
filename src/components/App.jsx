6f2ca406d456ff3494b6a0dfacec4c9e6d03aea0
DevAbabil <ababilhossain35@gmail.com>
2024-11-20 09:41:45 +0600

    welcome toast added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
+import { ToastContainer, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../styles/App.css";
 import Layout from "./Layout";
 import SecretKey from "./SecretKey";
 
 function App() {
   return (
-    <Layout>
-      <div className="text-center my-2 text-2xl text-[red] lg:hidden visible w-full max-w-full">Your device screen is not suitable for this application, please try to use larger screen.</div>
-      <SecretKey />
-    </Layout>
+    <div>
+      <Layout>
+        <div className="text-center my-2 text-2xl text-[red] lg:hidden visible w-full max-w-full">
+          Your device screen is not suitable for this application, please try to
+          use larger screen.
+        </div>
+        <SecretKey />
+      </Layout>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+    </div>
   );
 }
 
--- src/components/ViewsCounter.jsx
+++ src/components/ViewsCounter.jsx
@@ -1,22 +1,33 @@
 import SiteViews from "react-siteviews";
-import { memo, useEffect } from "react";
+import { memo } from "react";
 import loader from "../assets/loader.webp";
+import { toast, Bounce } from "react-toastify";
 
 const ViewsCounter = () => {
-  useEffect(() => {
-    console.log("ViewsCounter rendred");
-  }, []);
+  const visitedToast = () =>
+    toast("Welcome to Secret-Key app", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
+
   return (
     <div className="flex justify-center items-center gap-1 text-[rgba(var(--text))] text-sm font-bold">
       <span>App Visited:</span>
       <SiteViews
         projectName="my-project"
         visited={() => {
-          alert("Website visited");
+          visitedToast();
         }}
-        getData={(value) => {
-          console.log(value);
-        }}
+        // getData={(value) => {
+        //   console.log(value);
+        // }}
         refresh="10"
         placeHolder={<img src={loader} alt="loader" className="size-[17px]" />}
       />
